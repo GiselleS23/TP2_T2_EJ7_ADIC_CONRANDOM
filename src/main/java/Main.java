767aa5515dc2daeb8a0f3a326fab6e30dc6cfd97
GiselleS23 <128999381+GiselleS23@users.noreply.github.com>
2024-05-07 13:17:37 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,55 @@
+/* import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int numAle;
+    Random ran = new Random();
+    numAle = ran.nextInt(6)+1;
+    System.out.println(numAle);
+
+    scanner.close();
+  }
+
+} */
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+      //aqui genero los numerosaleatorios
+        Random random = new Random();
+        int dado1 = random.nextInt(6) + 1;
+        int dado2 = random.nextInt(6) + 1;
+        int dado3 = random.nextInt(6) + 1;
+ 
+        int seisObtenidos = 0;
+      //comparo si sale 6 en los dados
+        if (dado1 == 6) seisObtenidos++;
+        if (dado2 == 6) seisObtenidos++;
+        if (dado3 == 6) seisObtenidos++;
+      
+        String calificacion;
+        switch (seisObtenidos) {
+            case 3:
+                calificacion = "excelente";
+                break;
+            case 2:
+                calificacion = "muy bien";
+                break;
+            case 1:
+                calificacion = "regular";
+                break;
+            default:
+                calificacion = "pésimo";
+        }
+
+       
+        System.out.println("Dados lanzados: " + dado1 + ", " + dado2 + ", " + dado3);
+        System.out.println("Seis obtenidos: " + seisObtenidos);
+        System.out.println("Calificación: " + calificacion);
+    }
+}
